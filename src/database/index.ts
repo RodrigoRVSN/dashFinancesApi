39787ec7a3797466f57fb2ba36e60dd1a9b434dd
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-11-03 21:48:07 -0300

    🚀 teste deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,6 +6,9 @@ const client = new Client({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
